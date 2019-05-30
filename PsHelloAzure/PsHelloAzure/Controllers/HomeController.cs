a47f81610c2e4cb4f9253ffe3cc804ac1504958a
Marcelo Barsotti <mbarsott@gmail.com>
2019-05-30 16:59:30 -0400

    Test deployment slots. Deploy release to staging and master to production.

--- PsHelloAzure/PsHelloAzure/Controllers/HomeController.cs
+++ PsHelloAzure/PsHelloAzure/Controllers/HomeController.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using PsHelloAzure.Models;
+using System.Diagnostics;
 
 namespace PsHelloAzure.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = _configuration["Greeting"];
+            return View("Index", model);
         }
 
         public IActionResult About()

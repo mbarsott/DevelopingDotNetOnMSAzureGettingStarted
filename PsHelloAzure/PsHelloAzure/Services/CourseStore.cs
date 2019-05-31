d882d32ac97d63266cf5123137120b12a5b35da4
Marcelo Barsotti <mbarsott@gmail.com>
2019-05-30 21:52:59 -0400

    CosmosDB

---
+++ PsHelloAzure/PsHelloAzure/Services/CourseStore.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Documents.Client;
+using PsHelloAzure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsHelloAzure.Services
+{
+    public class CourseStore
+    {
+        private DocumentClient client;
+        private Uri coursesLink;
+
+        public CourseStore()
+        {
+            var uri = new Uri("https://pshelloazuredb.documents.azure.com:443/");
+            var key = "Je9HiLUWamLkosC1nomt9ztGlnDATGzitB1aL1Npyn7uSHKnRJ4XceOKJ5hebL6zoUQiiKlaKHJHdlwqHGyqUA==";
+            client = new DocumentClient(uri, key);
+            coursesLink = UriFactory.CreateDocumentCollectionUri("pshelloazuredb", "courses");
+        }
+
+        public async Task InsertCourses(IEnumerable<Course> courses)
+        {
+            foreach (var course in courses)
+            {
+                await client.CreateDocumentAsync(coursesLink, course);
+            }
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            var courses = client.CreateDocumentQuery<Course>(coursesLink)
+                                .OrderBy(c => c.Title);
+
+            return courses;
+        }
+    }
+}
